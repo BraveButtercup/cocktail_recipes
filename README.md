225eae74d80ca74a3f6e4b2acb9fad002a8141e2
bravebuttercup <batorbogi@gmail.com>
2024-06-12 19:02:50 +0200

    Readme updated, design changed, JS file commented

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
+# Cocktail Recipe 
 
-A cocktail website, where you can search by name and ingredients.
-I have used the APIs from the website https://www.thecocktaildb.com/api.php
+It's a simple website for cocktail recipes, where you can search by name and by ingredients of cocktails.
 
-Link to the website:
-https://bravebuttercup.github.io/cocktail_recipes/
+Link: https://bravebuttercup.github.io/cocktail_recipes
+
+
+
+## Technical information
+
+- **Frontend**: HTML, CSS and JavaScript.
+- **API:** Uses the CocktailDB API to fetch cocktail recipes.
+
+
+## Features
+
+- Search by name: The user can write the name of the recipe in the search field, and the recipes will be displayed on the right side.
+- Search by ingredients: The user can search by ingredients as well.
+- Rendering recipe details: For recipes searched by ingredients, users can view the details by clicking the "View recipe" button.
+
+
+## User guide
+
+  1. Open the website in your browser.
+  2. On the homepage  https://bravebuttercup.github.io/cocktail_recipes, choose whether you want to search by name or by ingredients.
+  3. Fill out the searh field. Empty input does not allowed, you will receive an error message.
+  4. Click the "Search" button.
+  5. If you searched by name, you can see the ingredients and the preparation steps.
+  6. If you searched by ingredients, you can click the "View recipe" button to see the details.
+
--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const UPARROW = document.querySelector(".backtotop");
 let showBackButton;
 let previousData = [];
 
+/**Fetching data with API  */
 async function fetchData(url) {
     try {
         const response = await fetch(url);
@@ -20,6 +21,7 @@ async function fetchData(url) {
     }
 };
 
+/*Rendering data on screen*/
 function renderData(data, showBackButton = false) {
 
     if (data.length) {
@@ -30,6 +32,7 @@ function renderData(data, showBackButton = false) {
     }
 };
 
+/*The template of the fetched data: with or without ingredients*/
 function createDataTemplate(data, showBackButton = false) {
 
     let filteredIngredients = renderIngredients(data);
@@ -56,6 +59,7 @@ function createDataTemplate(data, showBackButton = false) {
     }
 }
 
+/*Searchbar input validation*/
 function validation() {
 
     if (INPUT.value == "" || INPUT.value.trim().length == 0) {
@@ -67,6 +71,9 @@ function validation() {
     return true;
 };
 
+/* Handles the event triggered by a form submission or button click.
+  Prevents the default action, validates input, fetches data based on search type,
+  and renders the data or displays an error message if no results are found.*/
 async function eventHandler(e) {
 
     e.preventDefault();
@@ -88,16 +95,15 @@ async function eventHandler(e) {
             previousData = data;
             renderData(data);
             CONTAINER.classList.remove("viewed")
+            CONTAINER.classList.add("show-scrollbar")
         }
         catch (error) {
             ERROR_CONTAINER.innerHTML = `<div class="error-card"> No Results </div>`
         }
-
     }
-
-    CONTAINER.scrollIntoView();
 };
 
+/*If the fetched API contains ingredients,returned it in a list*/
 function renderIngredients(value) {
     let newList = [];
 
@@ -108,20 +114,21 @@ function renderIngredients(value) {
         };
     }
     return newList.map(newListIngr => `<li>${newListIngr}</li>`).join("");
-
 }
+
+/*Handles the click event on the view button to fetch and display detailed recipe information.*/
 async function viewRecipeDetails(e) {
 
     if (e.target.classList.contains('js-view-btn')) {
-        const drinkId = e.target.getAttribute('data-id');
+        const drinkId = e.target.getAttribute('data-id');// Get the id from the data attribute
         const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`;
         const data = await fetchData(url);
-        renderData(data, showBackButton = true);
-        CONTAINER.classList.add("viewed")
+        renderData(data, showBackButton = true);// fetch and display the detailed recipe and shows the back button
+        CONTAINER.classList.add("viewed");
     }
-    CONTAINER.scrollIntoView();
 }
 
+/*Clicking the back button displays all the searched items of the user, not just one */
 function goBack(e) {
     if (e.target.classList.contains('js-back-btn')) {
         renderData(previousData);
@@ -137,9 +144,9 @@ INPUT.addEventListener("keypress", (e) => {
     }
 });
 
-
 CONTAINER.addEventListener("click", viewRecipeDetails);
 CONTAINER.addEventListener("click", goBack);
+/*the "go-to-top" arrow is displayed only when the user scrolls down with 20px*/
 window.addEventListener("scroll", () => {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         UPARROW.classList.add("show");
